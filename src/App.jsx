b42ebf625002e03c0deb7c3aafc3b7b33f4b8264
Arsalan Ahmed <arsalanahmeddev1@gmail.com>
2023-11-15 10:22:14 +0500

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import styles from './style'
 import { Header, AppName, AddTodo, TodoItems } from './components'
+import { useState } from 'react'
 
 const App = () => {
-let todoItems = [
-  {
-    id: 1,
-    name: 'buy milk',
-    date: '11/11/23',
-  },
-  {
-    id: 2,
-    name: 'buy Egg',
-    date: '12/11/23',
-  },
-  {
-    id: 3,
-    name: 'buy sugar',
-    date: '13/11/23',
-  },
-]
+  let initiallTodoItems = [
+    {
+      id: 1,
+      name: 'buy milk',
+      date: '11/11/23',
+    },
+    {
+      id: 2,
+      name: 'buy Egg',
+      date: '12/11/23',
+    },
+    {
+      id: 3,
+      name: 'buy sugar',
+      date: '13/11/23',
+    },
+  ]
+  const [todoItems, setTodoItems] = useState(initiallTodoItems);
+
+  const handleNewTodo = (todoName, dueDate) => {
+    console.log(`new item added: ${todoName} Date:${dueDate}`);
+  }
+
   return (
     <>
       <Header />
-      <center className='mt-[40px] '>
+      <center className='mt-[40px]'>
         <AppName />
       </center>
-      <AddTodo />
-      <TodoItems todoItems={todoItems}/>
+      <AddTodo addNewTodo={handleNewTodo} />
+      <TodoItems todoItems={todoItems} />
 
     </>
   )
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import { useState } from "react";
 
-const AddTodo = () => {
+const AddTodo = ({addNewTodo}) => {
+  const [todoName, setTodoName] = useState()
+  const [dueDate, setDueDate] = useState()
+  const handletodoNameChange = (event) => {
+    setTodoName(event.target.value);
+  }
+  const handletodoDateChange = (event) => {
+    setDueDate(event.target.value);
+  }
   return (
     <div className="flex justify-center">
-      <input type="text" placeholder='Enter Todo Here' className='border border-[black]' />
-      <input type="date" className='border border-[black] ms-[10px]' />
-      <button className='bg-[green] px-[30px] py-[10px] rounded-md text-[white] ms-[10px]'>Add</button>
+      <input type="text" placeholder='Enter Todo Here'  className='border border-[black]' onChange={handletodoNameChange}/>
+      <input type="date" className='border border-[black] ms-[10px]' onChange={handletodoDateChange}/>
+      <button className='bg-[green] px-[30px] py-[10px] rounded-md text-[white] ms-[10px]' onClick={()=> addNewTodo('a','b')}>Add</button>
     </div>
   )
 }
 
-export default AddTodo
+export default AddTodo;
