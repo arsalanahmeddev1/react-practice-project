d22eecb66a946dd426ca5a71a16465b6f1b782f1
Arsalan Ahmed <arsalanahmeddev1@gmail.com>
2023-11-15 10:29:11 +0500

    creating a todolist

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
 const AddTodo = ({addNewTodo}) => {
-  const [todoName, setTodoName] = useState()
-  const [dueDate, setDueDate] = useState()
+  const [todoName, setTodoName] = useState('')
+  const [dueDate, setDueDate] = useState('')
   const handletodoNameChange = (event) => {
     setTodoName(event.target.value);
   }
   const handletodoDateChange = (event) => {
     setDueDate(event.target.value);
   }
+  const addTodoBtnClicked = () => {
+    addNewTodo(todoName, dueDate)
+    setTodoName('')
+    setDueDate('')
+  }
   return (
     <div className="flex justify-center">
-      <input type="text" placeholder='Enter Todo Here'  className='border border-[black]' onChange={handletodoNameChange}/>
-      <input type="date" className='border border-[black] ms-[10px]' onChange={handletodoDateChange}/>
-      <button className='bg-[green] px-[30px] py-[10px] rounded-md text-[white] ms-[10px]' onClick={()=> addNewTodo('a','b')}>Add</button>
+      <input type="text" placeholder='Enter Todo Here'  className='border border-[black]' onChange={handletodoNameChange} value={todoName}/>
+      <input type="date" className='border border-[black] ms-[10px]' onChange={handletodoDateChange} value={dueDate}/>
+      <button className='bg-[green] px-[30px] py-[10px] rounded-md text-[white] ms-[10px]' onClick={ addTodoBtnClicked}>Add</button>
     </div>
   )
 }
